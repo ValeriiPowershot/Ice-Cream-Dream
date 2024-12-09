511f5b95eefc0a5a1847ff1b4c1c7d059e0d770a
ValeriiCSharp <fox.vvb11@gmail.com>
2024-12-09 17:19:24 +0100

    Entry Point

---
+++ Assets/Scripts/Infrastructure/Game.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Mime;
+using Services.Input;
+using UnityEngine;
+
+namespace Infrastructure
+{
+    public class Game
+    {
+        public static IInputService InputService;
+
+        public Game()
+        {
+            if (Application.isEditor) 
+                InputService = new StandaloneInputService();
+        }
+    }
+}
